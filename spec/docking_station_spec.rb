0bb6012f59377bfbda75cc4172212dd33a7a1580
james <mail.jt@gmail.com>
2017-01-04 12:22:26 +0000

    step 11 final test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,8 +11,18 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
-  it 'shows currently docked bikes' do
+  it 'shows currently docked bikes when empty' do
+    expect(subject.view_bikes).to eq nil
+  end
+  it 'shows currently docked bikes when full' do
     bike = Bike.new
-    expect(subject.view_bikes).to eq false
+    subject.dock_bike(bike)
+    expect(subject.view_bikes).to eq bike
+  end
+  it 'shows no bikes when we remove the bike in it' do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    subject.release_bike
+    expect(subject.view_bikes).to eq nil
   end
 end

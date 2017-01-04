536ad419e547ecd8170ad38c7299ab1de1304a5b
Rob Brentnall <rob.brentnall@gmail.com>
2017-01-04 12:00:41 +0000

    Ex 11 - second test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,10 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike}
   it { is_expected.to respond_to :dock_bike }
+  it 'would accept dock' do
+    bike = Bike.new
+    expect(subject.docks_bike(bike)).to eq true
+  end
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working

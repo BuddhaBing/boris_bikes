dd3471b03d946c5d9865df10ad03cf52a78e1758
Rob Brentnall <rob.brentnall@gmail.com>
2017-01-04 13:13:24 +0000

    ex 12 error test

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,14 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
+  attr_reader :bike, :capacity
 
   def initialize
     @bike = Bike.new
+    @capacity = 1
   end
 
   def release_bike
-    #return Bike.new
     if @bike
       bike = @bike
       @bike = nil
@@ -19,9 +19,11 @@ class DockingStation
   end
 
   def dock_bike(bike)
+    raise "docking station full" if @bike
     @bike = bike
     true
   end
+
   def view_bikes
     @bike
   end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,7 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike}
   it { is_expected.to respond_to :dock_bike }
   it 'would accept dock' do
+    subject.release_bike
     bike = Bike.new
     expect(subject.dock_bike(bike)).to eq true
   end
@@ -19,6 +20,7 @@ describe DockingStation do
     expect(subject.view_bikes.class).to eq Bike
   end
   it 'shows no bikes when we remove the bike in it' do
+    subject.release_bike
     bike = Bike.new
     subject.dock_bike(bike)
     subject.release_bike
@@ -27,5 +29,9 @@ describe DockingStation do
   it 'raises an error when told to release a bike when docking station is empty' do
     subject.release_bike
     expect{subject.release_bike}.to raise_error("no bikes available")
+  end
+  it 'does not allow more bikes to be docked than the docking station\'s capacity' do
+    bike = Bike.new
+    expect{ subject.dock_bike(bike) }.to raise_error("docking station full")
   end
 end
